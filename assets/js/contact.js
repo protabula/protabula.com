1ac811cd9554515770a8e0b4b43a9092513d11be
ProTabula LLC <dan@protabula.com>
2016-02-15 16:32:52 -0600

    contact for is working with a big hack

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,55 +1,65 @@
 
-requirejs.config({
-    baseUrl: '/assets/js/vendor',
-    paths: {
-        'mailgun-js': 'mailgun-js/lib/mailgun',
-        'scmp' : 'scmp/index'
-        // 'crypto' : 'crypto'
-    }
-});
+
+var api_key = 'key-5beb175df9bfc3a220ce6390c3d6eda9';
+var domain = 'mg.protabula.com';
+
+Vue.config.delimiters = ['[[', ']]'];
+Vue.config.unsafeDelimiters = ['[[[', ']]]'];
 
 
-require(['mailgun-js'], function (mailgun) {
+new Vue({
+    el: '#contact',
+    data: {
+        email: '',
+        name: '',
+        title: '',
+        message: '',
+    },
+    methods: {
+        onSubmit: function () {
 
-    var api_key = 'key-5beb175df9bfc3a220ce6390c3d6eda9';
-    var domain = 'mg.protabula.com';
-    debugger;
-    var m = mailgun({ apiKey: api_key, domain: domain });
-
-    Vue.config.delimiters = ['[[', ']]'];
-    Vue.config.unsafeDelimiters = ['[[[', ']]]'];
+            if (this.validate()) {
 
 
-    new Vue({
-        el: '#contact',
-        data: {
-            email: '',
-            name: '',
-            title: '',
-            message: '',
-        },
-        methods: {
-            onSubmit: function () {
+                this.sendEmail();
 
-                if (this.validate()) {
+                this.updateGoogleForm();
 
-                    var data = {
-                        from: 'Excited User <me@samples.mailgun.org>',
-                        to: 'ace.poblete@gmail.com',
-                        subject: 'Hello',
-                        text: 'Testing some Mailgun awesomness!'
-                    };
-
-                    m.messages().send(data, function (error, body) {
-                        console.log(body);
-                    });
-
-                }
-            },
-
-            validate: function (index) {
-                return (this.email && this.name && this.message);
             }
+        },
+
+        sendEmail: function () {
+            var from = this.email + '<' + this.name + '>';
+
+            var data = {
+                from: from,
+                to: 'ace.poblete@gmail.com',
+                subject: 'Thanks for getting in touch!',
+                text: 'Testing some Mailgun awesomness!'
+            };
+        },
+
+        updateGoogleForm: function () {
+
+            var googleActionUrl = 'https://docs.google.com/a/protabula.com/forms/d/1MzIJU_77HQqSFQ2hvF1CfAcCsRqURwshRGEFtQONz-Y/formResponse';
+
+            var data = {
+                'entry.1605764209': this.email,
+                'entry.75873054': this.name,
+                'entry.1436883073': this.title,
+                'entry.1704065402': this.message
+            }
+
+            this.$http.post(googleActionUrl, data, function (results) {
+                debugger;
+            }, function (results) {
+                debugger;
+            });
+
+        },
+
+        validate: function (index) {
+            return (this.email && this.name && this.message);
         }
-    });
+    }
 });
---
+++ thank-you.md
@@ -0,0 +1,8 @@
+---
+layout: page
+title: Thank You!
+---
+
+# We have recieved your request for more information!
+
+## We will get back to you as soon as possible!
